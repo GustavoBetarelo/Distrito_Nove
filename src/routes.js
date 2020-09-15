492aea25207733621af2b810a7151774d6ea484f
AlessandroFresneda84 <alessandro.fresneda84@gmail.com>
2020-09-14 23:00:35 -0300

    Teste

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import Teste from './pages/teste';
+import Teste from './pages/teste/index.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +9,16 @@ const routes = [
   {
     path: '/teste',
     name: 'Teste',
+    component: Teste,
+  },
+  {
+    path: '/teste-1',
+    name: 'Teste1',
+    component: () => import('./components/HelloWorld.vue'),
+  },
+  {
+    path: '/teste-2',
+    name: 'Teste2',
     component: Teste,
   },
 ];
