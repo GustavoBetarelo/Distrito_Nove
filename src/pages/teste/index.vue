fe4d92eda5b0c2211e3769c98b9381d7d986c6f5
AlessandroFresneda84 <alessandro.fresneda84@gmail.com>
2020-09-14 23:18:35 -0300

    Teste

--- src/pages/teste/index.vue
+++ src/pages/teste/index.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="col-10 mx-auto">
-    TESTE
-    <v-layout class="col-3">
-      <v-flex>
-        TESTE
-      </v-flex>
-    </v-layout>
-  </div>
+  <v-container>
+    <div class="col-10 mx-auto">
+      TESTE
+      <v-layout class="col-3">
+        <v-flex>
+          TESTE
+        </v-flex>
+      </v-layout>
+    </div>
+  </v-container>
 </template>
 
 <script>
